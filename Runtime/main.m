928b9eef8150ea53f38e6295853b10eb04e04909
chenjunyu <chenjunyu@youmi.net>
2017-02-25 13:20:11 +0800

    Initial Commit

---
+++ Runtime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Runtime
+//
+//  Created by Jymn_Chen on 2017/2/25.
+//  Copyright © 2017年 com.jymnchen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
